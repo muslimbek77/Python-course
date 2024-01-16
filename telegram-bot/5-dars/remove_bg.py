3b2940589e9716f430738ea67dd2ded9bdc250f1
muslimbek77 <muslimjonbekali@mail.ru>
2024-01-16 20:15:33 +0500

    bg_image_file

--- telegram-bot/5-dars/remove_bg.py
+++ telegram-bot/5-dars/remove_bg.py
@@ -3,14 +3,18 @@ import requests
 
 def removebg(FILE_NAME):
     rasm=''
-    API_KEY ='dpa9g4v2Bf2TAW3AQjQTv77f'
+    API_KEY ='RMXXt43mVCBYYf6x9QPYMzbW'
 
 
     response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
     data={
         'image_url': FILE_NAME,
-        'size': 'auto'
+        'size': 'auto',
+        #'bg_image_url':'image/url'
+    },
+    files = {
+        #'bg_image_file':open("background.jpg","rb"),
     },
     headers={'X-Api-Key': API_KEY},
 )
